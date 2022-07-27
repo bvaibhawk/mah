57f9c54babd560e35863dc6f6bacb080d5e1896e
bvaibhawk <83122348+bvaibhawk@users.noreply.github.com>
2022-07-27 23:57:44 +0530

    Add files via upload

--- diamond.py
+++ diamond.py
@@ -673,7 +673,7 @@ def page1():
         #table
         if(df6['Location'][i]=='Table'):
             if(df6['Shape'][i]==shape and shape=='RO'): 
-                if((sizeprec>=df6['sizemin'][i]) & (cut==df6['cut'][i]) ):
+                if((sizeprec>=df6['sizemin'][i]) & (cut==df6['cut'][i]) & (sizeprec<=df6['sizemax'][i]) ):
                     if fluo!='None' and fluo!='Medium' and fluo!='Faint':
 
                         if xx==1:
