ce50c39e5ecf4a5932c454bb1291728bde1dace2
bvaibhawk <83122348+bvaibhawk@users.noreply.github.com>
2022-07-27 22:48:31 +0530

    Add files via upload

--- diamond.py
+++ diamond.py
@@ -72,7 +72,7 @@ def page1():
     col21, col22, col23, col24, col25 = st.columns(5)
     col26, col27, col28, col29, col30 = st.columns(5)
     col31, col32, col33, col34, col35 = st.columns(5)
-    col36, col37, col38, col49, col40 = st.columns(5)
+    col36, col37 = st.columns(2)
 
     # color_dict = {"D":10,"E":9,"F":8,"G":7,"H":6,"I":5,"J":4,"K":3,"L":2,"M":1}
     # clarity_dict = {"IF":8,"VVS1":7,"VVS2":6,"VS1":5,"VS2":4,"SI1":3,"SI2":2,"I1":1}
