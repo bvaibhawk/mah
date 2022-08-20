89ebb22245848c86088bfd71512e30c253869901
bvaibhawk <83122348+bvaibhawk@users.noreply.github.com>
2022-08-20 17:32:08 +0530

    Update Discount.py

--- Discount.py
+++ Discount.py
@@ -4258,7 +4258,7 @@ def calcDiscount(shape, szgr, color, clarity, cut, polish, symmetry, fluo, rap, 
         if temp - result > 15:
             result = temp - 15
             
-    ans[]={result,base}
+    ans=[result,base]
     return ans
 def get_cut_comments(min_diam, max_diam, tabl, height, ratio, col_shade, cr_angle,
                      cr_height, pv_angle, pv_depth, girdle_percentage, girdle_from, girdle_to, girdle_condition,
